711ebbeb786158399aeca2d3b2817b43fa9cb393
dwi312 <dwi31287no5@gmail.com>
2021-11-12 20:25:11 +0700

    Belajar mengenai konsep dasar OOP pada PHP yaitu CLASS dan OBJECT

---
+++ 1-Class-dan-Object/contoh.php
@@ -0,0 +1,22 @@
+<?php
+
+
+// Membuat Class
+class Coba
+{
+    // Property
+    public $a;
+
+    // Method
+    public function b()
+    {
+    }
+}
+
+
+// Membuat Object instance dari class
+$cobaObject1 = new Coba(); // => Contoh Object ke 1
+$cobaObject2 = new Coba(); // => Contoh Object ke 2
+
+var_dump($cobaObject1);
+var_dump($cobaObject1);
