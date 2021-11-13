6f5bc1d2b6597fc6495d81c0e7f375bd9db6ba3d
dwi312 <dwi31287no5@gmail.com>
2021-11-13 15:22:41 +0700

    Mempelajari mengenai CONSTANT atau KONSTANTA pada pemrograman Object Oriented PHP

---
+++ 10-Constant/latihan.php
@@ -0,0 +1,17 @@
+<?php
+// define('NAMA', 'Cungkring');
+
+// echo NAMA;
+// echo '<br>';
+
+// const UMUR = 17;
+// echo UMUR;
+
+
+class Coba
+{
+    const NAMA = 'Cungkring',
+        UMUR = 17;
+}
+
+echo Coba::NAMA . " " . Coba::UMUR;
