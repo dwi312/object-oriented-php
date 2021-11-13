a11d6faa23889ce44d2467955bc7aa5a862fb479
dwi312 <dwi31287no5@gmail.com>
2021-11-13 19:30:30 +0700

    Mempelajari pdan pmbapembahasan mengenai konsep KELAS ABSTRAK atau ABSTRACT CLASS pada pemrograman Object Oriented PHP.

--- 11-Abstract-Class/latihan.php
+++ 11-Abstract-Class/latihan.php
@@ -1,36 +1,184 @@
 <?php
 
-abstract class Buah
+// abstract class Buah
+// {
+//     private $warna;
+
+//     abstract public function makan();
+
+//     public function setWarna($warna)
+//     {
+//         $this->warna = $warna;
+//     }
+// }
+
+// class Apel extends Buah
+// {
+//     public function makan()
+//     {
+//         // code..
+//     }
+// }
+
+// class Jeruk extends Buah
+// {
+//     public function makan()
+//     {
+//         // code..
+//     }
+// }
+
+
+// $apel = new Apel();
+// $apel->makan();
+
+// $apel = new Buah();
+// $apel->makan();
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+abstract class Produk
 {
-    private $warna;
-
-    abstract public function makan();
-
-    public function setWarna($warna)
+    private $judul, $penulis, $penerbit, $harga, $diskon;
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
     {
-        $this->warna = $warna;
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function setJudul($judul)
+    {
+        $this->judul = $judul;
+    }
+
+    public function getJudul()
+    {
+        return $this->judul;
+    }
+
+    public function setPenulis($penulis)
+    {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+
+    public function setPenerbit($penerbit)
+    {
+        $this->penerbit = $penerbit;
+    }
+
+    public function getPenerbit()
+    {
+        return $this->penerbit;
+    }
+
+    public function setHarga($harga)
+    {
+        $this->harga = $harga;
+    }
+
+    public function getHarga()
+    {
+        return $this->harga;
+    }
+
+    public function setDiskon($diskon)
+    {
+        $this->diskon = $diskon;
+    }
+
+    public function getDiskon()
+    {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    abstract public function getInfoProduk();
+
+    public function getInfo()
+    {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        return $str;
     }
 }
 
-class Apel extends Buah
+
+class Komik extends Produk
 {
-    public function makan()
+    public $jmlHalaman;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0)
     {
-        // code..
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "Komik : " . $this->getInfo() . " - {$this->jmlHalaman} Halaman.";
+        return $str;
     }
 }
 
-class Jeruk extends Buah
+class Game extends Produk
 {
-    public function makan()
+    public $waktuMain;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0)
     {
-        // code..
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+
+        $this->waktuMain = $waktuMain;
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "Game : " . $this->getInfo() . " ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+class CetakInfoProduk
+{
+    public $daftarProduk = [];
+
+    public function tambahProduk(Produk $produk)
+    {
+        $this->daftarProduk[] = $produk;
+    }
+
+    public function cetak()
+    {
+        $str = "DAFTAR PRODUK : <br>";
+
+        foreach ($this->daftarProduk as $p) {
+            $str .= "- {$p->getInfoProduk()} <br>";
+        }
+
+        return $str;
     }
 }
 
 
-$apel = new Apel();
-$apel->makan();
 
-$apel = new Buah();
-$apel->makan();
+// Object instance dari class
+$produk1 = new Komik("Dragon Ball Z", "Akira Toriyama", "Bandai", 35000, 100);
+$produk2 = new Game("Dino Crisis 3", "Dajiro Kato", "Namco", 175000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+
+echo $cetakProduk->cetak();
